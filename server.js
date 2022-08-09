f8df72ffb5adf6cba0dc5814b82f48cc93233d70
mgondolesi <mgondolesi@gmail.com>
2022-08-09 14:40:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
+  response.write('Hello Mariano Gondolesi, como te va?\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
